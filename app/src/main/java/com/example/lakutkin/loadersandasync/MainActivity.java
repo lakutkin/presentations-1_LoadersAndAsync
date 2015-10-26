112ef1175bcaa319c8f54af1bca6be526eaab208
unknown <lakutkin@torrance-new.mercury.office>
2015-10-26 15:36:24 +0400

    Добавил базовый класс для лоадера
    new Loader()

--- app/src/main/java/com/example/lakutkin/loadersandasync/MainActivity.java
+++ app/src/main/java/com/example/lakutkin/loadersandasync/MainActivity.java
@@ -1,15 +1,35 @@
 package com.example.lakutkin.loadersandasync;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
+	private static class MyLoader extends TrueAsyncLoader<String> {
+
+		public MyLoader(Context context) {
+			super(context);
+		}
+
+		@Override
+		public String loadInBackground() {
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			return "Test";
+		}
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,35 +38,23 @@ public class MainActivity extends AppCompatActivity {
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
-		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
-			}
-		});
+		getSupportLoaderManager().initLoader(0, null, this);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
+	public Loader<String> onCreateLoader(int id, Bundle args) {
+		return new MyLoader(this);
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
+	public void onLoadFinished(Loader<String> loader, String data) {
+		TextView textView = (TextView) findViewById(R.id.textView);
+		textView.setText(data);
 	}
+
+	@Override
+	public void onLoaderReset(Loader<String> loader) {
+
+	}
+
 }
