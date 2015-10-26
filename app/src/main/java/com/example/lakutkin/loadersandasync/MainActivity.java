89b1ebcc9c0ca9349e14433268b4819427f962bb
unknown <lakutkin@torrance-new.mercury.office>
2015-10-26 15:41:44 +0400

    Почему ничего не работает?

--- app/src/main/java/com/example/lakutkin/loadersandasync/MainActivity.java
+++ app/src/main/java/com/example/lakutkin/loadersandasync/MainActivity.java
@@ -2,19 +2,15 @@ package com.example.lakutkin.loadersandasync;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
-	private static class MyLoader extends TrueAsyncLoader<String> {
+	private static class MyLoader extends AsyncTaskLoader<String> {
 
 		public MyLoader(Context context) {
 			super(context);
